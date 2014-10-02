54597b4944b902aa74de85cdf83bbed1420f0f32
Ross Boberg <ross.boberg@moorecap.com>
2014-10-02 11:55:43 -0400

    finished p 3.6

--- MIDS/FieldExperiments/PS2/ps2.R
+++ MIDS/FieldExperiments/PS2/ps2.R
@@ -1,13 +1,14 @@
-setwd("C:/Users/Ross/Documents/GitHub/rross/MIDS/FieldExperiments/PS2")
+#setwd("C:/Users/Ross/Documents/GitHub/rross/MIDS/FieldExperiments/PS2")
+setwd("F:/Docs/Personal/rross/MIDS/FieldExperiments/PS2")
 library(ggplot2)
 
-####
-# Setup Problem 3.6
+#########################
+### Problem 3.6
 
 p36_df <- read.csv('ex3_6_data.csv')
 n <- c(448,510)
 p36_df <- transform(p36_df, y0 = round(y0/sum(y0)*n[1]), y1 = round(y1/sum(y1)*n[2]))
-act_ate <- with(p36_df, sum((y0/sum(y0) - y1/sum(y1))*rating))
+act_ate <- with(p36_df, sum((y1/sum(y1) - y0/sum(y0))*rating))
 
 p36_df$all <- p36_df$y0 + p36_df$y1
 
@@ -19,8 +20,25 @@ run_sim <- function(subjects, n){
   return(ate)
 }
 
+iter = 10000
 est_ate <- replicate(10000, run_sim(subjects, n))
 
-ecdf(est_ate)(act_ate)
+# how many bigger?
+n_bigger = sum(est_ate >= act_ate)
+n_bigger
+
+# one-tailed p-value estimate
+n_bigger/iter
+
+# how many bigger abs?
+n_bigger_abs = sum(abs(est_ate) >= act_ate)
+n_bigger_abs
+
+# two-tailed p-value estimate
+n_bigger_abs/iter
 
 ggplot(data.frame(ATE = est_ate), aes(x=ATE)) + geom_density() + geom_vline(xint=act_ate, col='blue')
+
+
+#############################
+###
