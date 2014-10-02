a8734e68d8393b9d972d9cf810d998988531e6f5
Ross Boberg <ross.boberg@moorecap.com>
2014-10-02 16:51:15 -0400

    finish most of 3.8

--- MIDS/FieldExperiments/PS2/ps2.R
+++ MIDS/FieldExperiments/PS2/ps2.R
@@ -15,6 +15,19 @@ run_sim <- function(subjects, n0, n1){
   return(ate)
 }
 
+calc_est_se <- function(group, outcome){
+  y0 <- outcome[group==0]
+  y1 <- outcome[group==1]
+  m <- length(y1)
+  N <- m + length(y0)
+  v0 <- var(y0)
+  v1 <- var(y1)
+  return(sqrt(v0/(N-m) + v1/m))
+}
+
+calc_est_ate <- function(group, outcome){
+   return(mean(outcome[group==1]) - mean(outcome[group==0]))
+}
 
 #########################
 ### (1) Problem 3.6
@@ -84,3 +97,77 @@ sum(all_possible(c(y0,y1-7), length(y1)) >= mean(y1-7) - mean (y0))
 #############################
 ### (3) Problem 3.8
 
+# load data as data frame w/ 3 columns
+# st denotes the state: tx or ar
+# group is 0 (4 year term) or 1 (2 year term)
+# bills is the number of bills introduced
+
+p38_df <- read.csv('ex3_8_data.csv')
+
+est_ate <- c(
+  ar = with(subset(p38_df, st == 'ar'), calc_est_ate(group, bills)),
+  tx = with(subset(p38_df, st == 'tx'), calc_est_ate(group, bills))
+  )
+
+est_se <- c(
+  ar = with(subset(p38_df, st == 'ar'), calc_est_se(group, bills)),
+  tx = with(subset(p38_df, st == 'tx'), calc_est_se(group, bills))
+  )
+
+n_st <- c(
+  ar=sum(p38_df$st == 'ar'),
+  tx=sum(p38_df$st == 'tx')
+  )
+
+calc_block_ate <- function(ates, ns){
+  return(sum(ates * ns) / sum(ns))
+}
+
+block_ate <- calc_block_ate(est_ate, n_st)
+
+# Why just pooling would be biased?
+# Equal numbers of control in each group
+# but more treatment in Arkansas. Because AR senators
+# typically introduced fewer bills, this would bias the average 
+# bills introduced by the treatment group lower and
+# exagerate the negative treatment effect.
+
+total_ate_se <- sqrt(
+  est_se['ar']^2 * n_st['ar'] / nrow(p38_df) + 
+    est_se['tx']^2 * n_st['tx'] / nrow(p38_df)
+  )
+
+# Randomization Inference w/ blocking?
+# Need to randomize by block!
+
+# function to cacluate random inference with blocks
+# n_bg is just a table of blocks and groups
+# it's ncessary in the function but faster to calculate once before hand
+# instead of every time
+
+run_block_sim <- function(block, group, outcome, n_bg = table(block, group)){
+  ates <- c()
+  ns <- c()
+  for(blocki in unique(block)){
+    assign <- assign_treat(n_bg[blocki,'0'], n_bg[blocki,'1'])
+    ates[blocki] <- calc_est_ate(assign, outcome[block == blocki])
+    ns[blocki] <- sum(n_bg[blocki,])
+  }
+  
+  return(calc_block_ate(ates, ns))
+}
+
+block <- p38_df$st
+group <- p38_df$group
+outcome <- p38_df$bills
+n_bg <- table(block, group)
+
+iter = 10000
+rand_ate <- replicate(iter, run_block_sim(block, group, outcome, n_bg))
+sum(rand_ate > abs(block_ate)) / iter
+
+ggplot(data.frame(ATE = rand_ate), aes(x=ATE)) + geom_density() + geom_vline(xint=block_ate, col='blue')
+
+
+#############################
+### (4) Problem 3.11
