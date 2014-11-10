0f2e1570a27ccfd41f6a5f0828d1af351cf0b9e5
Ross Boberg <ross.boberg@moorecap.com>
2014-11-10 16:16:58 -0500

    start ps4

---
+++ MIDS/FieldExperiments/PS4/PS4.R
@@ -0,0 +1,19 @@
+####################
+# ROSS BOBERG
+# FIELD EXPERIMENTS
+# PROBLEM SET 4
+####################
+
+####################
+### Functions & Libraries
+
+# set wd
+setwd('F:/Docs/Personal/rross/MIDS/FieldExperiments/PS4')
+#setwd('C:/Users/Ross/Documents/R/rross/MIDS/FieldExperiments/PS4')
+#setwd('C:/Users/Ross/Documents/GitHub/rross/MIDS/FieldExperiments/PS4')
+
+#####################
+### Problem 3 - Exercise 5.10
+
+# load data
+
